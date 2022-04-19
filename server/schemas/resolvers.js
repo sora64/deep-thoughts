1b3aa5a0b6ef4b769bb82f30216ad70b4e9c724a
Alan Phelps <phelpsa64@gmail.com>
2022-04-19 17:52:02 -0400

    added Mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -24,6 +24,14 @@ const resolvers = {
         .populate("thoughts");
     },
   },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+
+      return user;
+    },
+    login: async () => {},
+  },
 };
 
 module.exports = resolvers;
